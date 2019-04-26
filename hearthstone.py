3e0636d7a1395d39fa67e3eeeed19da85b85bcfb
Yifan Cao <amadeuscao.cs@gmail.com>
2019-04-25 20:39:12 -0500

    finish first part

--- hearthstone.py
+++ hearthstone.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import bs4
 import requests
@@ -8,6 +9,7 @@ from bokeh.io import output_file, show
 from bokeh.palettes import Category20c
 from bokeh.plotting import figure
 from bokeh.transform import cumsum
+import matplotlib.pyplot as plt
 
 #function to obtain archetype, percent of game and win rate information, then use panda to put those data in a table
 def getTable(url, index):
@@ -27,7 +29,6 @@ def processData(data):
         if(currId[0:-6] == ''):
             continue
         decks.append(currId[0:-6])
-    print(len(decks))
     return decks
 
 ## count number of each archetypes appearing in the individual rank
@@ -35,7 +36,6 @@ def getNum(archetype, decks):
     counts = []
     archelist = []
     sumCount = 0
-    print(len(decks))
     for arche in archetype:
         archelist.append(arche)
         count = 0
@@ -48,6 +48,25 @@ def getNum(archetype, decks):
     archelist.append('others')
     return archelist, counts
 
+def horizontalBar(archelist, counts):
+    # set values in tuples
+    temp = list(zip(archelist, counts))
+    #sort the values in tuples
+    temp.sort(key=lambda tup: tup[1], reverse=False)
+    archesort = []
+    nums = []
+    # get the sorted num
+    for pair in temp:
+        archesort.append(pair[0])
+        nums.append(pair[1])
+    y_pos = np.arange(len(archelist))
+    # Create horizontal bars
+    plt.barh(y_pos, nums)
+    # Create names on the y-axis
+    plt.yticks(y_pos, archesort)
+    plt.show()
+
+
 # plot the pie chart with given data
 def pieChart(archelist,  counts):
     output_file("pie.html")
@@ -55,14 +74,11 @@ def pieChart(archelist,  counts):
     percentage = []
     for i in counts:
         sumValue += i
-
     for i in counts:
         percentage.append((i*100)/sumValue)
-
     ## sort the tuple list by second value
     temp = list(zip(archelist, percentage))
     temp.sort(key=lambda tup: tup[1], reverse=True)
-
     values = {
         temp[0][0]: temp[0][1],
         temp[1][0]: temp[1][1],
@@ -86,20 +102,15 @@ def pieChart(archelist,  counts):
         temp[19][0]: temp[19][1],
         temp[20][0]: temp[20][1],
     }
-
     # set colors of pie chart
     colors = Category20c[len(temp)-1]
     colors.append('#cdaef2')
     # set data, angle and color
     data = pd.Series(values).reset_index(name='percent').rename(columns={'index':'decks'})
     data['angle'] = data['percent']/100*2*pi
-    angles = data['angle'].copy()
-    angles.sort_values(ascending = False, inplace = True)
     data['color'] = colors
-
     p = figure(plot_width = 700, plot_height = 600, title="percentage of hearthstone archetypes in top 154 winning decks", toolbar_location=None,
             tools="hover", tooltips="@decks: @percent%",  x_range=(-5, 8))
-
     p.wedge(x=0, y=1, radius=4,
             start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
             line_color="white", fill_color='color', legend='decks', source=data)
@@ -116,12 +127,12 @@ def main():
     archetype = overallData['Archetype']
     # indiviudal archetype win rate, eliminate the last unamed row    
     individualData = getTable(url, 1)
-    print(individualData[:-1])
     decks = processData(individualData)
     archelist, nums = getNum(archetype, decks)
-    ## draw plot then 
+    #plot the horizontal bar
+    horizontalBar(archelist, nums) 
+    # plot the pie chart
     pieChart(archelist, nums)
-
 
 if __name__ == "__main__":
     main()
